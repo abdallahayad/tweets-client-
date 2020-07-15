f8f6568bdbb6544970a05c79dc5be3d7339f48ae
abdallahayad <abdallahayad2298@gmail.com>
2020-07-15 22:39:02 +0200

    updated some layout issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,10 +9,7 @@ const reducers = combineReducers({ user: userReducer });
 const store = createStore(
   reducers,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
